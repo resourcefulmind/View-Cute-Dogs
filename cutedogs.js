04b9b37453830b5900c488f60ebc6b90c4dd1282
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2021-01-13 09:22:52 +0100

    --- Auto-Git Commit ---

--- cutedogs.js
+++ cutedogs.js
@@ -40,19 +40,30 @@ async function init() {
     // now we have images of dogs present, but we need to fix the situation such that when the user selects another breed of dogs, it will give them that specific breed they want to see and to do that we have to listen for events
 
     // add an event listener to listen for when the user selects another breed
+    pickBreed.addEventListener("change", handleBreedChange);
+    // handleBreedchange is a function we will write soon below to target the event we want to change and determine what we want the system to do when we change the event
 
+    // but first we need a code that will make the page not just stay stagnant when another picture is selected and the system is fetching from the API, we write that below
 
+    main.addEventListener("load", function() {
+        main.classList.add("show");
+        loader.classList.remove("show");
+    });
+}
 
+// now we write the function for handleBreed like we stated in line 44
 
+async function handleBreedChange(event) {
+    const breed = event.target.value;
 
+    main.classList.remove("show");
+    loader.classList.add("show");
 
+    const list = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
 
+    const listJson = await list.json();
 
-
-
-
-
-
+    main.src = listJson.message;
 }
 
 init();
